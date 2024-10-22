84b3a79ca03de573985f1d672ea513a24a9041a9
LtMilligan <gioele.miscia@gmail.com>
2024-10-22 10:22:15 +0200

    [UPD] Milestone2 done

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/index', [ProjectController::class, 'index'])->name('guest_home');
+Route::get('/index', [ProjectController::class, 'index'])->name('project_home');
